40a94d57d6919932705e6008f7797922d25648ea
suman kumar <sumankumar91593@gmail.com>
2025-09-04 11:52:49 +0530

    Complete All operation in singlyLL

--- LinkedList/Reivise/SinglyLL/basic.cpp
+++ LinkedList/Reivise/SinglyLL/basic.cpp
@@ -12,6 +12,17 @@ public:
         this->data = data;
         this->next = NULL;
     }
+
+    ~Node()
+    {
+        int value = this->data;
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "Memory is free with data is this: " << value << endl;
+    }
 };
 
 void insertAtHead(Node *&head, int data)
@@ -28,25 +39,58 @@ void insertAtTail(Node *&tail, int data)
     tail = temp;
 }
 
-void insertAtMiddle(Node* &head,Node* &tail, int position,int data){
-    if(position == 1){
-        insertAtHead(head,data);
+void insertAtMiddle(Node *&head, Node *&tail, int position, int data)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, data);
         return;
     }
-    Node* temp = head;
+    Node *temp = head;
     int cnt = 1;
-    while(cnt < position - 1){
-        temp = temp -> next;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
         cnt++;
     }
-    if(temp -> next == NULL){
-        insertAtTail(tail,data);
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, data);
         return;
     }
 
-    Node * nodeToInsert = new Node(data);
-    nodeToInsert -> next = temp ->next;
-    temp -> next = nodeToInsert;
+    Node *nodeToInsert = new Node(data);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+
+void deleteNode(Node *&head, Node *&tail, int position)
+{
+    // first Position
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        // memory free for firt position
+        temp->next = NULL;
+        delete temp;
+        return;
+    }
+
+    // delete middle or last node of the linkedlist
+    Node *prev = NULL;
+    Node *curr = head;
+    int cnt = 1;
+    while (cnt < position)
+    {
+        prev = curr;
+        curr = curr->next;
+        cnt++;
+    }
+    prev->next = curr->next;
+    curr->next = NULL;
+    delete curr;
+    tail = prev;
 }
 
 void print(Node *&head)
@@ -68,19 +112,21 @@ int main()
     Node *head = node1;
     Node *tail = node1;
     print(head);
-    insertAtHead(head, 15);   // 20 15 10   20 15 10 51
+    insertAtHead(head, 15); // 20 15 10   20 15 10 51
     print(head);
     insertAtHead(head, 20);
     print(head);
-    insertAtTail(tail,51);
+    insertAtTail(tail, 51);
     print(head);
-    insertAtMiddle(head,tail, 3,111);
+    insertAtMiddle(head, tail, 3, 111);
     print(head);
-    insertAtMiddle(head,tail, 1,156);
+    insertAtMiddle(head, tail, 1, 156);
     print(head);
-    insertAtMiddle(head,tail,7,158);
+    insertAtMiddle(head, tail, 7, 158);
     print(head);
-    cout << "Head: " << head -> data << endl;
-    cout << "Tail: " << tail -> data << endl;
+    deleteNode(head, tail, 7);
+    print(head);
+    cout << "Head: " << head->data << endl;
+    cout << "Tail: " << tail->data << endl;
     return 0;
 }
